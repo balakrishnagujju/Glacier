30463f9ce2d7a924e26753c7c71f5c799766be55
balakrishnagujju <balakrishna.gujju@outlook.com>
2019-06-27 21:51:40 +0530

    Initial Commit

---
+++ src/main/java/forest.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class forest {
+        public static void main(String... args) throws IOException {
+
+
+                if(args.length==0){
+                    System.out.println("Hello world");
+                }else {
+                    throw new IOException();
+                }
+
+        }
+
+}
---
+++ src/test/java/testForest.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+//import org.junit.jupiter.api.Test;
+
+public class testForest {
+
+    private forest toTest = new forest();
+
+    /*@Test
+    public void maintest() {
+        IOException result = forest.main();
+        assertEquals(9907, result);
+    }*/
+}
+
